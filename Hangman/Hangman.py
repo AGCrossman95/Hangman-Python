a3889feb61105146653a42d0285efc9c7bb5f924
Austin Crossman <88110892+AGCrossman95@users.noreply.github.com>
2022-03-02 09:24:25 -0600

    Add files via upload

---
+++ Hangman/Hangman.py
@@ -0,0 +1,9 @@
+import random
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+def hangman():
+    word
